a0de4399a42e078f82a1c2cab8a18b2821597a1f
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2025-05-19 11:22:48 +0200

    Add files via upload

---
+++ embeddedPythonDevice.py
@@ -0,0 +1,294 @@
+import RPi.GPIO as GPIO
+import time
+import socketio
+import threading
+import sys
+import os
+import queue
+from RPLCD.i2c import CharLCD
+import datetime
+import json
+import spidev
+import bluetooth
+
+
+#Search for the tag 'NEEDS IMPLEMENTATION' to check if anything needs to be coded.
+
+
+#Defining variables
+PlantID = '682aeec2b1ddfd70fed67e96' #This should be sent from the server
+MoistureLimit = 20 #This should be sent from the server
+AllowedDryPeriod = 0 #This should be sent from the server
+RequiredWaterAmount = 0 #This should be sent from the server
+LastWatered = 0
+WaterStatus = 0
+NewInput = False
+Metric = {}
+InitializationDone = False #Bool used for checking initialization
+SetupDone = False #Bool used for checking if connection is set up
+Url = 'http://10.176.69.183:3000' #This should be sent from the server
+MessageQueue = queue.Queue()
+
+#Defining messages
+MessageStartUp = 'Welcome! Initialization starting now.'
+MessageInitDone = 'Initialization done! Monitoring has begun.'
+MessageConnectingSocketIO = 'Connected to server.'
+MessageDisconnect = 'Disconnected from server.'
+MessageLowWaterTank = 'The water tank is low, please fill it up.'
+MessagePumpingWater = 'Watering plant.'
+MessageImplementNewSettings = 'Implementing new settings. Please wait.'
+MessageResetProgram = 'Restarting the application now.'
+MessageShutdown = 'Shutting down.'
+
+#PIN setup
+GREEN_PIN = 0
+BOARD_SCL_PIN = 0
+BOARD_SDA_PIN = 0
+RESET_BUTTON_PIN = 21
+WATER_SENSOR_PIN = 16
+PUMP_PIN = 24
+
+#GPIO setup
+GPIO.setwarnings(False)          # Slår GPIO advarsler fra
+GPIO.setmode(GPIO.BCM)           # Bruger Broadcom pin-nummerering
+
+# Initialisering af output-pins
+
+GPIO.setup(PUMP_PIN, GPIO.OUT)
+GPIO.output(PUMP_PIN, GPIO.HIGH)
+
+# Initialisering af input-pins
+GPIO.setup(RESET_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(WATER_SENSOR_PIN, GPIO.IN)
+
+#Utility files
+SETTINGS_FILE = "plantsettings.json"
+
+#Hardware setup
+client = socketio.Client()
+lcd = CharLCD('PCF8574', 0x27)
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 1350000
+
+#Function implementation
+#SocketIO functions
+@client.event
+def connect():
+    queue_message(message=MessageConnectingSocketIO, duration=2)
+    client.emit('join_room', PlantID)
+
+@client.event
+def disconnect():
+    queue_message(message=MessageDisconnect, duration=2)
+
+@client.on("send_iot_conf")
+def save_settings(config):
+    with open(SETTINGS_FILE, "w") as f:
+        json.dump(config, f, indent=2)
+    load_settings()
+
+def start_socketio():
+    client.connect(Url)
+    client.wait()
+
+def send_metric(metric):
+    client.emit('send_metric', metric)
+
+#Utility functions
+def init_display():
+    lcd.clear()
+
+def scroll_message(message, duration, lcd, delay=0.3):
+    if len(message) <= 32:
+        lcd.clear()
+        lcd.write_string(message[:16])  # Linje 1
+        if len(message) > 16:
+            lcd.cursor_pos = (1, 0)
+            lcd.write_string(message[16:32])  # Linje 2
+        time.sleep(duration)
+    else:
+        padded_msg = message + " " * 16
+        total_steps = len(padded_msg) - 15
+        start_time = time.time()
+
+        while time.time() - start_time < duration:
+            for i in range(total_steps):
+                if time.time() - start_time >= duration:
+                    break
+                lcd.clear()
+                lcd.write_string(padded_msg[i:i+16])
+                time.sleep(delay)
+
+
+def queue_handler():
+    while not exit_event.is_set():
+        if not MessageQueue.empty():
+            message, duration = MessageQueue.get()
+            scroll_message(message, float(duration), lcd)
+            lcd.clear()
+            MessageQueue.task_done()
+
+def queue_message(message, duration):
+    MessageQueue.put((message, duration))
+
+def metric_creator(PlantId, TimeStamp, Moisture, LastWatered, WaterStatus):
+    return {
+        "roomId": PlantId,
+        "entity": {
+            "customPlantId": PlantID,    
+            "timeStamp": TimeStamp,
+            "moisture": Moisture,
+            "lastWatered": LastWatered,
+            "waterStatus": WaterStatus
+        }
+    }
+
+def load_settings():
+    with open(SETTINGS_FILE, "r") as f:
+        settings = json.load(f)
+    global PlantID; PlantID = settings["plantId"]
+    global MoistureLimit; MoistureLimit = settings["moisture"]
+    global AllowedDryPeriod; AllowedDryPeriod = settings["allowedDryPeriod"]
+    global RequiredWaterAmount; RequiredWaterAmount = settings["requiredWaterAmount"]
+
+def hard_reset_program():
+    while True:
+        if GPIO.input(RESET_BUTTON_PIN) == GPIO.LOW:
+            StartTime = time.time()
+            while GPIO.input(RESET_BUTTON_PIN) == GPIO.LOW:
+                time.sleep(0.1)
+                if time.time() - StartTime >= 3:
+                    queue_message(message=MessageResetProgram, duration=3)
+                    time.sleep(3)
+                    shutdown(restart=True)
+        time.sleep(0.1)  
+
+def clear_display():
+    lcd.clear()
+
+def shutdown(restart=False):
+    queue_message(message=MessageShutdown, duration=5)
+    GPIO.cleanup()
+    time.sleep(5)
+    exit_event.set()  # Signal the queue_handler thread to exit
+    lcd.clear()
+    spi.close()
+    lcd.close()
+    time.sleep(2)
+    if restart:
+        os.execv(sys.executable, [sys.executable] + sys.argv)
+    else:
+        sys.exit(0)
+
+#Water tank functions
+def measure_water_level():
+    return GPIO.input(WATER_SENSOR_PIN)
+    
+def check_water_tank():
+    if measure_water_level() < 1:
+        global WaterStatus
+        WaterStatus = 0
+        queue_message(message=MessageLowWaterTank, duration=8)
+
+def pump_on():
+    GPIO.output(PUMP_PIN, GPIO.LOW)
+
+def pump_off():
+    GPIO.output(PUMP_PIN, GPIO.HIGH)
+
+def pump_water(duration):
+    pump_on()
+    queue_message(message=MessagePumpingWater, duration=duration)
+    time.sleep(duration)
+    pump_off()
+    check_water_tank() 
+
+#Moisture functions
+def read_channel(channel: int) -> int:
+    if not 0 <= channel <= 7:
+        raise ValueError("Channel must be 0-7")
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+def get_soil_moisture() -> int:
+    return read_channel(0)
+
+def measure_moisture() -> float:
+    raw = get_soil_moisture()
+    percent = 100 - ((raw / 1023) * 100)
+    return round(percent, 1)
+
+#Threads setup
+message_queue_thread = threading.Thread(target=queue_handler, daemon=True)
+listener_thread = threading.Thread(target=start_socketio, daemon=True)
+reset_thread = threading.Thread(target=hard_reset_program, daemon=True)
+exit_event = threading.Event()
+
+def setupConnection():
+    global Url
+    global SetupDone
+    init_display()
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    port = 1  # Standard port for RFCOMM
+    server_sock.bind(("", port))
+    server_sock.listen(1)
+
+    lcd.write_string(f"Waiting for Bluetooth connection on RFCOMM channel {port}")
+
+    client_sock, client_info = server_sock.accept()
+    lcd.write_string(f"Accepted connection from {client_info}")
+    time.sleep(2)
+    try:
+        data = client_sock.recv(1024)  # Receive up to 1024 bytes
+        if data:
+            Url = data.decode('utf-8')
+            lcd.write_string(f"Received URL: {Url}")
+    finally:
+        SetupDone = True
+        lcd.clear()
+        client_sock.close()
+        server_sock.close()
+
+def initialization():
+    global InitializationDone
+    init_display()
+    #load_settings()
+    message_queue_thread.start()
+    reset_thread.start()
+    listener_thread.start()
+    queue_message(message=MessageStartUp, duration=3)
+    InitializationDone = True
+    
+    
+def loop():
+    global MoistureLimit
+    global PlantID
+    global LastWatered
+    global WaterStatus
+    while True:
+        time.sleep(10)
+        
+        MoistureMeasurement = measure_moisture()
+        print(f"Moisture Measurement: {MoistureMeasurement}")  # Debug line
+        CurrentDate = datetime.datetime.now().isoformat()
+        try:
+            if MoistureMeasurement < MoistureLimit:
+                print("Moisture below limit. Activating pump.")  # Debug line
+                pump_water(2)
+                LastWatered = datetime.datetime.now().isoformat()
+            else:
+                check_water_tank()
+            send_metric(metric_creator(PlantId=PlantID, TimeStamp=CurrentDate, Moisture=MoistureMeasurement, LastWatered=LastWatered, WaterStatus=WaterStatus))
+        except Exception as e:
+            print(e)
+            client.send(f"Exception: {e}")
+
+
+if __name__ == "__main__":
+    # if SetupDone == False:
+    #     setupConnection()
+    if InitializationDone == False:
+        initialization()
+    loop()
